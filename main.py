961eb0e00f1db72cedda1b51f4c7dd26f39f849d
heoyoon25 <heoyoon25@gmail.com>
2025-09-15 21:59:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ st.header('두 숫자 더하기')
 col1, col2 = st.columns(2)
 
 with col1:
-    A = st.text_input('첫 번째 숫자 (A)를 입력하세요:', value="0", key='A_input')
+    A = st.text_input('첫 번째 숫자 (A)를 입력하세요:', key='A_input')
 with col2:
-    B = st.text_input('두 번째 숫자 (B)를 입력하세요:', value="0", key='B_input')
+    B = st.text_input('두 번째 숫자 (B)를 입력하세요:', key='B_input')
 try:
-    num_A = float(A)
-    num_B = float(B)
+    num_A = int(A)
+    num_B = int(B)
     sum_result = num_A + num_B
     st.success(f'두 수의 합은?{sum_result}')
 
