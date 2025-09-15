e5a037686c34635be7b07105de3fcb7a2622c3a4
heoyoon25 <heoyoon25@gmail.com>
2025-09-15 21:31:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ st.title('연산')
 st.header('A와 B 더하기')
 A = st.text_input('A를 입력하세요:', key= 'A_input')
 B = st.text_input('B를 입력하세요:', key= 'B_input')
-result = float(A) + float(B)
+num_A = float(A)
+num_B = float(B)
+result = num_A + num_B
 st.success(f'A와 B의 합은?{result}')
