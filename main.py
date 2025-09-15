58102c8115e16b4e1125aaf19f04cace797215ec
heoyoon25 <heoyoon25@gmail.com>
2025-09-15 21:58:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ with col2:
 try:
     num_A = float(A)
     num_B = float(B)
-
-sum_result = num_A + num_B
-st.success(f'두 수의 합은?{sum_result}')
+    sum_result = num_A + num_B
+    st.success(f'두 수의 합은?{sum_result}')
 
 except ValueError:
     st.error('유효한 숫자를 입력해 주세요')
