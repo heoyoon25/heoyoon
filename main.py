6fdfa48e687c7bb8bed4b8642fca38f221906db8
heoyoon25 <heoyoon25@gmail.com>
2025-09-15 21:25:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import streamlit as st
 st.title('연산')
 
 st.header('A와 B 더하기')
-with col1:
-  A = st.text_input('A를 입력하세요:', key= 'A_input')
-with col2:
-  B = st.text_input('B를 입력하세요:', key= 'B_input')
+A = st.text_input('A를 입력하세요:', key= 'A_input')
+B = st.text_input('B를 입력하세요:', key= 'B_input')
 result = A + B
 st.success(f'A와 B의 합은?{result}')
