7cfb2194ed51b6b2597d062ac640d9f64971a3cf
heoyoon25 <heoyoon25@gmail.com>
2025-09-15 21:55:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ with col1:
     A = st.text_input('첫 번째 숫자 (A)를 입력하세요:', value="0", key='A_input')
 with col2:
     B = st.text_input('두 번째 숫자 (B)를 입력하세요:', value="0", key='B_input')
-num_A = float(A)
-num_B = float(B)
+try:
+    num_A = float(A)
+    num_B = float(B)
+
 sum.result = num_A + num_B
 st.success(f'두 수의 합은?{sum.result}')
 
+except ValueError:
+    st.error('유효한 숫자를 입력해 주세요')
