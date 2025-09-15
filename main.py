06f56736a98b898698c1e98bf6dbf7294cef32d2
heoyoon25 <heoyoon25@gmail.com>
2025-09-15 20:56:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import streamlit as st
-
-st.title("title")
-st.header("header")
-st.markdown("""
-amrkdown
-**markdown**
-*markdown*
-""")
-st.write("hello!world")
-
+st.title('A,B 숫자 합하기')
+st.write('A와 B의 값을 더해줍니다.')
+A = int(input('첫번째 숫자: '))
+B = int(input('두번째 숫자: '))
+result = A+B
+print('두 숫자의 합은?',result)
